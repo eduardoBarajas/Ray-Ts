6c336611e554c79de7d6c22aa56f2d0ef8beb9d7
eduardoBarajas <barajas.eduardo@uabc.edu.mx>
2018-12-22 16:02:06 -0800

    Se agrego la actividad para controlar el registro de usuarios y el diseño para mostrar el perfil de los usuarios.

---
+++ app/src/main/java/com/barajasoft/raites/Activities/RegisterUserActivity.java
@@ -0,0 +1,20 @@
+package com.barajasoft.raites.Activities;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.barajasoft.raites.R;
+
+public class RegisterUserActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //deshabilita el menu del fondo definido en la clase padre
+        disableBottomMenu();
+        disableViewPager();
+        disableDrawer();
+        View layout = LayoutInflater.from(this).inflate(R.layout.about_activity,null);
+        addContent(layout);
+    }
+}
